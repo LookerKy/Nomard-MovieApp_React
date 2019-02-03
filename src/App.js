e88d498d6aa37f6bcfeb5bdf5701190b8b22e9af
LookerKy <nagaraz07@gmail.com>
2019-02-03 17:15:12 +0900

    fetch / component dumb smart

--- src/App.js
+++ src/App.js
@@ -3,24 +3,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title : "신과함께 인과연",
-    poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/9/97/%EC%8B%A0%EA%B3%BC_%ED%95%A8%EA%BB%98_-_%EC%9D%B8%EA%B3%BC_%EC%97%B0.png/250px-%EC%8B%A0%EA%B3%BC_%ED%95%A8%EA%BB%98_-_%EC%9D%B8%EA%B3%BC_%EC%97%B0.png"
-  },
-  {
-    title : "광해",
-    poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/a/a4/%EA%B4%91%ED%95%B4%2C_%EC%99%95%EC%9D%B4_%EB%90%9C_%EB%82%A8%EC%9E%90.jpg/250px-%EA%B4%91%ED%95%B4%2C_%EC%99%95%EC%9D%B4_%EB%90%9C_%EB%82%A8%EC%9E%90.jpg"
-  },
-  {
-    title : "아쿠아맨",
-    poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/d/d2/%EC%95%84%EC%BF%A0%EC%95%84%EB%A7%A8_%EC%98%81%ED%99%94.jpg/250px-%EC%95%84%EC%BF%A0%EC%95%84%EB%A7%A8_%EC%98%81%ED%99%94.jpg"
-  },
-  {
-    title : "범죄도시",
-    poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/8/87/%EB%B2%94%EC%A3%84%EB%8F%84%EC%8B%9C_2017.jpg/250px-%EB%B2%94%EC%A3%84%EB%8F%84%EC%8B%9C_2017.jpg"
-  } 
-]
 
 /*component 의 life cycle 
   render : componentWillMount() --> render() --> componentDidMount()
@@ -30,12 +12,82 @@ const movies = [
 
 class App extends Component {
   //state 가 변경될 때 마다 render가 새로 생성 될 것이다.
+  //example state change
+  /*state = {
+    // movies : [
+    //   {
+    //     title : "신과함께 인과연",
+    //     poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/9/97/%EC%8B%A0%EA%B3%BC_%ED%95%A8%EA%BB%98_-_%EC%9D%B8%EA%B3%BC_%EC%97%B0.png/250px-%EC%8B%A0%EA%B3%BC_%ED%95%A8%EA%BB%98_-_%EC%9D%B8%EA%B3%BC_%EC%97%B0.png"
+    //   },
+    //   {
+    //     title : "광해",
+    //     poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/a/a4/%EA%B4%91%ED%95%B4%2C_%EC%99%95%EC%9D%B4_%EB%90%9C_%EB%82%A8%EC%9E%90.jpg/250px-%EA%B4%91%ED%95%B4%2C_%EC%99%95%EC%9D%B4_%EB%90%9C_%EB%82%A8%EC%9E%90.jpg"
+    //   },
+    //   {
+    //     title : "아쿠아맨",
+    //     poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/d/d2/%EC%95%84%EC%BF%A0%EC%95%84%EB%A7%A8_%EC%98%81%ED%99%94.jpg/250px-%EC%95%84%EC%BF%A0%EC%95%84%EB%A7%A8_%EC%98%81%ED%99%94.jpg"
+    //   },
+    //   {
+    //     title : "범죄도시",
+    //     poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/8/87/%EB%B2%94%EC%A3%84%EB%8F%84%EC%8B%9C_2017.jpg/250px-%EB%B2%94%EC%A3%84%EB%8F%84%EC%8B%9C_2017.jpg"
+    //   },
+    //   {
+    //     title : "토이 스토리",
+    //     Qposter : "http://mblogthumb4.phinf.naver.net/20150721_167/yessok104_1437478621705X7aor_JPEG/toy_story_wallpaper_by_cepillo16.jpg?type=w2"
+    //   } 
+    // ]
+  }
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        // movies : [
+        //   {
+        //     title : "토이 스토리",
+        //     poster : "http://mblogthumb4.phinf.naver.net/20150721_167/yessok104_1437478621705X7aor_JPEG/toy_story_wallpaper_by_cepillo16.jpg?type=w2"
+        //   },
+        //   ...this.state.movies
+        // ]
+        movies : [
+          {
+            title : "신과함께 인과연",
+            poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/9/97/%EC%8B%A0%EA%B3%BC_%ED%95%A8%EA%BB%98_-_%EC%9D%B8%EA%B3%BC_%EC%97%B0.png/250px-%EC%8B%A0%EA%B3%BC_%ED%95%A8%EA%BB%98_-_%EC%9D%B8%EA%B3%BC_%EC%97%B0.png"
+          },
+          {
+            title : "광해",
+            poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/a/a4/%EA%B4%91%ED%95%B4%2C_%EC%99%95%EC%9D%B4_%EB%90%9C_%EB%82%A8%EC%9E%90.jpg/250px-%EA%B4%91%ED%95%B4%2C_%EC%99%95%EC%9D%B4_%EB%90%9C_%EB%82%A8%EC%9E%90.jpg"
+          },
+          {
+            title : "아쿠아맨",
+            poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/d/d2/%EC%95%84%EC%BF%A0%EC%95%84%EB%A7%A8_%EC%98%81%ED%99%94.jpg/250px-%EC%95%84%EC%BF%A0%EC%95%84%EB%A7%A8_%EC%98%81%ED%99%94.jpg"
+          },
+          {
+            title : "범죄도시",
+            poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/8/87/%EB%B2%94%EC%A3%84%EB%8F%84%EC%8B%9C_2017.jpg/250px-%EB%B2%94%EC%A3%84%EB%8F%84%EC%8B%9C_2017.jpg"
+          },
+          {
+            title : "토이 스토리",
+            poster : "http://mblogthumb4.phinf.naver.net/20150721_167/yessok104_1437478621705X7aor_JPEG/toy_story_wallpaper_by_cepillo16.jpg?type=w2"
+          }, 
+        ]
+      })
+    }, 2000);
+  }*/
+  state = {}
+  componentDidMount(){
+    console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating'));
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title = {movie.title} poster = {movie.poster} key = {index}/>
+    })
+    return movies;
+  }
+
   render() {
     return (
       <div className="App">
-        {movies.map((movie, index) => {
-          return <Movie title = {movie.title} poster = {movie.poster} key = {index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
 //movie card component
-class Movie extends Component {
-
-    static propTypes = {
-        title : PropTypes.string.isRequired,
-        poster : PropTypes.string.isRequired
-    }
-
-    render () {
-        return (
-            <div className="Movies">
-            <MoviePoster poster = {this.props.poster}/>
-            <h1>{this.props.title}</h1>
+const Movie = ({title, poster}) =>{
+    return (
+        <div className="Movies">
+            <MoviePoster poster = {poster}/>
+            <h1>{title}</h1>
             </div>
-        );
-    }
+    )
 }
 
-//image component
-class MoviePoster extends Component {
-    static propTypes = {
-        poster : PropTypes.string.isRequired
-    }
-    render () {
-        return (
-            <img src={this.props.poster} alt ='she is good'/>
-        );
-    }
+//image dumb component
+const MoviePoster = ({poster}) => {
+    return (
+        <img src={poster} alt ='Movie poster'/>
+    )
+}
+
+Movie.propTypes = {
+    title : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster : PropTypes.string.isRequired
 }
 
 export default Movie;
